3399941b8d71d02d8303c1d2f721a88ebf649845
root <muneerahmed00916@gmail.com>
2024-09-29 22:57:43 +0530

    footer edit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,9 @@ import { Button } from "@/components/ui/button";
 import MemoriesTable from "@/components/MemoriesTable";
 import { auth, signOut } from "@/auth";
 import { redirect } from "next/navigation";
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
+import { TwitterIcon } from "@/components/ui/icons";
+
 // import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export const runtime = 'edge';
@@ -53,6 +56,16 @@ export default async function DashboardPage() {
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">MindR.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <Link href={`https://github.com/MuneerAhmed03/mindr`} target="_blank" rel="noreferrer"
+            className="flex items-center justify-center hover: p-1"
+          >
+            <GitHubLogoIcon className="w-4 h-4 hover:scale-110" />
+          </Link> 
+          <Link href={`https://x.com/mun_err`} target="_blank" rel="noreferrer"
+            className="flex items-center justify-center hover: p-1"
+          >
+            <TwitterIcon className="w-4 h-4 hover:scale-110" />
+          </Link> 
           <Link
             className="text-xs hover:underline underline-offset-4"
             href="/tos"
--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -3,7 +3,11 @@ import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card"
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion"
 import { Brain, MessageCircle, Zap} from "lucide-react"
+import { GitHubLogoIcon } from '@radix-ui/react-icons'
+import { TwitterIcon } from "@/components/ui/icons";
+
 import Link from "next/link"
+
 
 // export const runtime = "edge";
 
@@ -170,6 +174,16 @@ export default function Component() {
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">MindR.</p>
         <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <Link href={`https://github.com/MuneerAhmed03/mindr`} target="_blank" rel="noreferrer"
+            className="flex items-center justify-center hover: p-1"
+          >
+            <GitHubLogoIcon className="w-4 h-4 hover:scale-110" />
+          </Link> 
+          <Link href={`https://x.com/mun_err`} target="_blank" rel="noreferrer"
+            className="flex items-center justify-center hover: p-1"
+          >
+            <TwitterIcon className="w-4 h-4 hover:scale-110" />
+          </Link> 
           <Link className="text-xs hover:underline underline-offset-4" href="/tos">
             Terms of Service
           </Link>
