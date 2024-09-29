ffe964f8eb2be49a364377bd0a83940f89927b8b
root <muneerahmed00916@gmail.com>
2024-09-29 23:03:25 +0530

    footer edit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -55,7 +55,7 @@ export default async function DashboardPage() {
       </main>
       <footer className="flex flex-col gap-2 sm:flex-row py-6 w-full shrink-0 items-center px-4 md:px-6 border-t">
         <p className="text-xs text-gray-500 dark:text-gray-400">MindR.</p>
-        <nav className="sm:ml-auto flex gap-4 sm:gap-6">
+        <nav className="sm:ml-auto flex items-center justify-center gap-4 sm:gap-6">
         <Link href={`https://github.com/MuneerAhmed03/mindr`} target="_blank" rel="noreferrer"
             className="flex items-center justify-center hover: p-1"
           >
